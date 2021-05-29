959c1bb8cc10844f8b25d27518ffdd3c1768ff28
itay zohar <dracula753@gmail.com>
2021-05-29 12:13:06 +0300

    ....

--- playground.R
+++ playground.R
@@ -1,6 +1,30 @@
 library(tidyverse)
-fake = readr::read_csv(file="C:/tmp/Fake.csv")
-true = readr::read_csv(file="C:/tmp/True.csv")
+#data15 = readr::read_csv(file="data/2015.csv")
+#data16 = readr::read_csv(file="data/2016.csv")
+data17 = readr::read_csv(file="data/2017.csv")
+data18 = readr::read_csv(file="data/2018.csv")
+data19 = readr::read_csv(file="data/2019.csv")
+data20 = readr::read_csv(file="data/2020.csv")
+data21 = readr::read_csv(file="data/2021.csv")
+
+glimpse(data20)
+View(data20)
+
+
+allData = c(data17, data18, data19, data20, data21)
+
+
+IsraelData = filter(data17, country == "Israel") %>% mutate(year=year)
+
+year = 18
+for (data in alldata){
+  data = filter(data.entry(),  == "Israel") %>% mutate(year=year)
+  IsraelData = bind_rows(IsraelData, data)
+  year = year + 1
+  
+}
+
+
 
 
 glimpse(true)
