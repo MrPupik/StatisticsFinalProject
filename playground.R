74c8007e857e76cdcfe577f9671a004980745177
itay zohar <dracula753@gmail.com>
2021-06-06 16:45:52 +0300

    add images

--- playground.R
+++ playground.R
@@ -61,35 +61,78 @@ for (i in c(1:length(AllData))) {
   
 }
 
-data15 = AllData[1]
-data15 = AllData[2]
-data15 = AllData[1]
-# t.test()
 
-
-
-
-# 
-# IsraelData15 = data15 %>% select(`Happiness Score`,`Economy (GDP per Capita)`)
  entire_data = bind_rows(AllData[[1]],AllData[[2]],AllData[[3]], AllData[[4]], AllData[[5]], AllData[[6]], AllData[[7]])
- IsraelData = entire_data %>% filter(country == 'Israel')
+ some_data = bind_rows(AllData[[1]],AllData[[4]], AllData[[7]])
+ 
+ # factor
+ entire_data = entire_data %>% mutate(year = factor(year))
+ some_data = some_data %>% mutate(year = factor(year))
+
+ entire_data = entire_data %>% mutate(isIsrael = ifelse(country == "Israel", 1, 0))
+ some_data = some_data %>% mutate(isIsrael = ifelse(country == "Israel", 1, 0))
 #   filter(data15, Country == "Israel") %>% mutate(year=2015)
 
-entire_data %>% ggplot(aes(x=freedom, y=score, color=as.factor(year)))  + geom_point() + scale_color_hue(h = c(150, 250))
+ 
+entire_data %>% ggplot(aes(x=gdp, y=score, color=year))  + geom_point() # gdp - all
+some_data %>% ggplot(aes(x=gdp, y=score, color=year, shape=factor(isIsrael)))  + geom_point() # gdp - some data
+AllData[[7]] %>% ggplot(aes(x=gdp, y=score, color=year))  + geom_point() # gdp - only 21
+some_data %>% ggplot(aes(y=score, x=factor(year) ,fill=year))  + geom_boxplot() # boxplot - compare var and mean
+
 
  
-entire_data %>% ggplot(aes(x=(gdp), y=score, color=as.factor(year)))  + geom_point()
-AllData[[7]] %>% ggplot(aes(x=(gdp), y=score, color=as.factor(year)))  + geom_point()
- 
-# entire_data %>% ggplot(aes(x=`score`)) + geom_density()
+some_data %>% ggplot(aes(x=`score`, color=year)) + geom_density()
 
 
 View(AllData[[6]])
 # ggplot(data, aes(x=))
 
- # entire_data %>% ggplot(aes(x=score, color=year))  + geom_histogram(bins = 30) 
+ entire_data %>% ggplot(aes(y=score, x=year))  + geom_boxplot()
+ some_data %>% ggplot(aes(y=score, x=year))  + geom_boxplot()
+ 
+ 
+
+ score15 = AllData[[1]] %>% select(country, score) 
+ score18 = AllData[[3]] %>% select (country, score) 
+ score21 = AllData[[7]] %>% select (country, score) %>% rename(score.21 = score)
+
+diff_data = inner_join(x=score15, y=score18, by="country", suffix = c(".15", ".18"))
+diff_data = inner_join(x=diff_data, y=score21, by="country")
+diff_data = diff_data %>% mutate(diff.18 = score.18-score.15) %>% mutate(diff.21 = score.21-score.18) %>%
+          mutate(diff.total = score.21-score.15)
+diff_total = diff_data %>% select(country, diff.total)
+data21 = inner_join(AllData[[7]], diff_total, by="country")
 
 
+data21 %>% ggplot(aes(x=score, y=diff.total)) + geom_point() + geom_line()
 
+scoreLevel = function (scores){
+ sapply(scores, function(scores){
+   if (scores < 4){
+     return('low')
+   }
+   if(scores<6){
+     return('medium')
+   }
+   return('high')
+ })
+  
+}
 
+data21 = data21 %>% mutate(score_level = scoreLevel(data21$score)) %>%
+        mutate(score_level = factor(score_level, levels = c('low', 'medium', 'high')))
+
+data21 %>% ggplot(aes(x=score_level, y=diff.total)) + geom_boxplot()
+
+data21 %>% group_by(score_level) %>% summarise(mean=mean(diff.total))
+mean(filter(data21,score_level=='high')$diff.total)
+
+world = world %>% rename(country=region)
+world = inner_join(world, data21, by="country")
+world %>% count(world$region)
+library(maps)
+world = map_data('world') 
+
+ggplot(world) + 
+  geom_map(mapping=aes(long, lat, map_id=region), map=world) + ggtitle("who gets better") 
 
